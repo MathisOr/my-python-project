5586ca482878478282ed85c115b8c7ea756a2bfd
MathisOr <118972520+MathisOr@users.noreply.github.com>
2022-11-24 10:57:05 +0100

    Add files via upload

---
+++ my-python-project/Basic_Test.py
@@ -0,0 +1,29 @@
+import unittest
+import Person
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+    def test_failing(self):
+        a = 'titi'
+        b = 'toto'
+        self.assertEqual(a, b)
+
+class TestingPerson(unittest.TestCase):
+    def test_id(self):
+        person = Person.Person()
+        first_id = person.set_name('Alice')
+        self.assertEqual(first_id, 0)
+        second_id = person.set_name('Bob')
+        self.assertEqual(second_id, 1)
+
+if __name__ == '__main__':
+    unittest.main()
