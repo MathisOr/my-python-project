5268e540ac534b4edf9120fdb78e188579479f96
MathisOr <118972520+MathisOr@users.noreply.github.com>
2022-11-24 15:24:37 +0100

    Update Basic_Test.py

--- my-python-project/Basic_Test.py
+++ my-python-project/Basic_Test.py
@@ -24,7 +24,7 @@ class TestingPerson(unittest.TestCase):
         self.assertEqual(first_id, 0)
         second_id = person.set_name('Bob')
         self.assertEqual(second_id, 1)
-        self.assertEquam(first_id, second_id)
+        
 
 if __name__ == '__main__':
     unittest.main()
